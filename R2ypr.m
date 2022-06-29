73247287c8f66512cf0f2f661027ade396db2206
kleist <61907235+KleistvonLiu@users.noreply.github.com>
2022-06-29 21:24:19 +0200

    change MATLAB Rq into liub Rq
    change the convention of rotation matrix and quaternion from matlab(robotic) into liub(areospace)

--- R2ypr.m
+++ R2ypr.m
@@ -1,5 +1,6 @@
 function angle = R2ypr(R)
     % sequence: Z Y X
+    % should be the same used in vins, so R uses robotic's convention
     yaw = atan2(R(2,1),R(1,1));
     pitch = atan2(-R(1,3),R(1,1)*cos(yaw)+R(1,2)*sin(yaw));
     roll = atan2(R(1,3) * sin(yaw) - R(2,3) * cos(yaw), -R(1,2) * sin(yaw) + R(2,2) * cos(yaw));
